e59fab4e0315716b8bb6157b301e82502cca2043
baebb <me@rossdyson.com>
2022-02-17 23:51:05 +0900

    mobile styling

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import React, { useState } from 'react'
 import ReactPlayer from 'react-player'
-import { Container, Row, Col, ListGroup, Nav } from 'react-bootstrap'
+import { Container, Row, Col, ListGroup, Nav, Card } from 'react-bootstrap'
 
 import { videos } from '../utils/vars'
 
@@ -20,47 +20,62 @@ export default function Home() {
         <title>Sy Dyson</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Container>
-        <h1 className="logo-text">
-          Sy Dyson
-        </h1>
-        <Nav
-          className="justify-content-center"
-          onSelect={(selectedKey) => console.log(`selected ${selectedKey}`)}
-          activeKey="/home"
-        >
-          <Nav.Item>
-            <Nav.Link href="/home">Home</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link eventKey="link-1">Link</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link eventKey="link-2">Link</Nav.Link>
-          </Nav.Item>
-        </Nav>
-        <p className="subhead-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt</p>
-        <Container className="videobox">
-          <Row>
-            <Col sm={9}>
-              <ReactPlayer
-                playing={videoPlaying}
-                url={videoUrl}
-                controls={true}
-              />
-            </Col>
-            <Col sm={3}>
-              <ListGroup variant="flush">
-                {videos.map(({ title, url }, index) => (
-                  <ListGroup.Item key={index} action onClick={() => handleVideoSelect(url)}>
-                    {title}
-                  </ListGroup.Item>
-                ))}
-              </ListGroup>
-            </Col>
-          </Row>
+      <Row>
+        <Container>
+          <h1 className="logo-text">
+            Sy Dyson
+          </h1>
+          <Nav
+            className="justify-content-center"
+            onSelect={(selectedKey) => console.log(`selected ${selectedKey}`)}
+            activeKey="/home"
+          >
+            <Nav.Item>
+              <Nav.Link href="/home">Home</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="link-1">Link</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="link-2">Link</Nav.Link>
+            </Nav.Item>
+          </Nav>
+          <p className="subhead-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt</p>
+          <div className="videobox">
+            <Row>
+              <Col xs={12} md={9} className="video-col">
+                <div className="player-wrapper">
+                  <ReactPlayer
+                    className="react-player"
+                    playing={videoPlaying}
+                    url={videoUrl}
+                    controls={true}
+                    width="100%"
+                    height="100%"
+                  />
+                </div>
+              </Col>
+              <Col xs={12} md={3}>
+                <Card>
+                  <ListGroup variant="flush" className="video-list">
+                    {videos.map(({ title, url }, index) => (
+                      <ListGroup.Item
+                        active={videoUrl === url}
+                        key={index}
+                        action
+                        onClick={() => handleVideoSelect(url)}
+                        className="video-list-item"
+                      >
+                        {title}
+                      </ListGroup.Item>
+                    ))}
+                  </ListGroup>
+                </Card>
+              </Col>
+            </Row>
+          </div>
         </Container>
-      </Container>
+      </Row>
     </Container>
   )
 }
